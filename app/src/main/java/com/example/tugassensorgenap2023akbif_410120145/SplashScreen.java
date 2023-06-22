81ce32771b205d28d5289980aec352ade7b1fc42
DimasMb <dmsmb770@gmail.com>
2023-06-22 22:58:42 +0700

    Komitmen Awal

---
+++ app/src/main/java/com/example/tugassensorgenap2023akbif_410120145/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.example.tugassensorgenap2023akbif_410120145;
+/**
+ * NIM: 10120145
+ * Nama : Dimas Muhamad Bhakti
+ * Kelas : IF-4
+ */
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 3000L); //3000 L = 3 detik
+    }
+}
